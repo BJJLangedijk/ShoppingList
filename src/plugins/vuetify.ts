ed21ff062ba305404773709645bea18ca6674a95
Bart Langedijk <bjjlangedijk@gmail.com>
2020-09-14 23:18:18 +0200

    Use static MDI font

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,11 @@
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+      iconfont: 'mdi', // default - only for display purposes
+    },
 });
