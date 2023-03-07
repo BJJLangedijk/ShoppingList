fd52b87d9264930af5354081426c5083642c0214
Bart Langedijk <bart.langedijk@vodafoneziggo.com>
2023-03-07 13:12:35 +0100

    revert to original primary color

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,6 +5,17 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
+export default createVuetify({
+    theme: {
+        defaultTheme: 'myCustomTheme',
+        themes: {
+            myCustomTheme: {
+                colors: {
+                    primary: '#1976d2'
+                }
+            }
+        }
+    }
+}
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 )
