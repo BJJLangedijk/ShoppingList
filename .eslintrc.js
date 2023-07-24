ac439dd1b8fd0328476f0ac428b7ee1a1ff94c03
Bart Langedijk <bjjlangedijk@gmail.com>
2023-07-24 16:15:49 +0200

    feat: Convert to Vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,13 @@ module.exports = {
     root: true,
     env: {
       node: true,
+      es2022: true
     },
     extends: [
       'plugin:vue/essential',
       '@vue/airbnb',
       '@vue/typescript/recommended',
     ],
-    parserOptions: {
-      ecmaVersion: 2020,
-    },
     rules: {
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
